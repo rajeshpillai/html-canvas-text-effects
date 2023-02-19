a790449e6d596e85841d2b6d8edc006f29e11b88
Rajesh Pillai <rajesh@algorisys.com>
2023-02-19 20:54:02 +0530

    Drawing, styling and positioning text

--- script.js
+++ script.js
@@ -7,4 +7,42 @@ window.addEventListener("load", function () {
   // DO NOT set canvas width/height with CSS as it stretch the drawing surface area and distort your drawings.
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  // Drawing style and positioning text
+
+  // Draw axes lines
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = "red";
+  ctx.beginPath();
+  ctx.moveTo(canvas.width/2, 0);
+  ctx.lineTo(canvas.width/2, canvas.height);
+  ctx.stroke();
+
+  ctx.strokeStyle = "green";
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height/2);
+  ctx.lineTo(canvas.width, canvas.height/2);
+  ctx.stroke();
+
+  const text = "Hello!";
+  const textX = canvas.width/2;
+  const textY = canvas.height/2;
+  
+  
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "orangered";
+  
+  ctx.font = "80px Helvetica";
+
+  // start, center, end
+  ctx.textAlign = "center";
+
+  // top, bottom, ideographic, middle, alphabetic
+  ctx.textBaseline = 'alphabetic';
+
+  ctx.fillText(text, textX, textY);
+  ctx.strokeText(text, textX, textY);
+  
+
+
 });
